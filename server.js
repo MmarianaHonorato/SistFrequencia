f5c2550fbe5bb06b1518029fd2ffaf0d59bd24e6
Mariana Honorato <mariana.cardoso14.mari@gmail.com>
2026-06-17 19:17:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor iniciado");
