f35dbbfb18c10e935554ae85d276bc6c8b3a75f9
Mariana Honorato <mariana.cardoso14.mari@gmail.com>
2026-06-17 19:15:34 -0300

    Use environment variable for server port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-console.log("Servidor iniciado");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor iniciado");
 });
